e188c26f74b4885dc70db83d13a560aa31cf8f9f
Maame Apenteng <MaameAJ@10.11.115.49>
2016-12-02 07:19:47 -0500

    fixed the api issues probably

--- api.php
+++ api.php
@@ -1,12 +1,12 @@
 <?php
-
+include_once '/models/API.class.php';
 // Requests from the same server don't have a HTTP_ORIGIN header
 if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
     $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
 }
 
 try {
-    $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    $API = new  DecalApi($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
     echo $API->processAPI();
 } catch (Exception $e) {
     echo json_encode(Array('error' => $e->getMessage()));
